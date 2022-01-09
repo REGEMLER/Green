0e8c23f04d520164904e728a7b45fa9e95870e9f
Yar <yaroslav239@gmail.com>
2022-01-09 14:32:27 +0300

    added hamburger

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+const toggler = document.getElementById(`toggle`);
+const menu = document.getElementById(`header-list-1`);
+toggler.addEventListener(`click`, () =>{
+    if( menu.style.opacity === `1`){
+        menu.style.height = `0`;
+        menu.style.visibility = `hidden`;
+        menu.style.opacity = `0`;
+
+    } else{
+        menu.style.height = `10rem`;
+        menu.style.visibility = `visible`;
+        menu.style.opacity = `1`;
+    }
+})
